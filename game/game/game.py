3daedc2582bc3a3885f11d9229ad16a6bea2d84b
xyd <513619669@qq.com>
2017-05-09 00:27:14 +0800

    添加几个类

--- game/game/game.py
+++ game/game/game.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 
-class World:
-    '世界类，负责与玩家线程交互，包含npc，怪物等所有玩家共享的全局信息'
+import users
+import monster
+
+class World(object):
+    """世界类，负责与玩家线程交互，包含npc，怪物等所有玩家共享的全局信息"""
     def __init__(self):
         self.monsters = {}
+        self.user = users.Users()
---
+++ game/game/monster.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+class Monster(object):
+    """怪,属性包括血量,位置等
+    控制怪的行为：追踪，攻击，随机移动等"""
+
+
